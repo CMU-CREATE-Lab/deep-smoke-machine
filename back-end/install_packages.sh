0aa50992ca689941a33f91905e6d6fa222ae3325
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2019-05-28 18:39:38 -0400

    Remove the installation of pytorch from install_packages.sh.

--- back-end/install_packages.sh
+++ back-end/install_packages.sh
@@ -6,9 +6,6 @@ pip install requests==2.21.0
 # OpenCV
 pip install opencv-python==4.1.0.25
 
-# pytorch
-conda install pytorch torchvision cudatoolkit=9.0 -c pytorch
-
 # For plotting images
 pip install matplotlib==3.0.3
 
