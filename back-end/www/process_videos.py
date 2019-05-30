18aeb239acb746d053301da6d87a1147527b43a5
SeanPrendi <sean.prendi@gmail.com>
2019-05-30 16:02:42 -0400

    Refactor code.

--- back-end/www/process_videos.py
+++ back-end/www/process_videos.py
@@ -18,28 +18,32 @@ def main(argv):
 
     op = OpticalFlow()
 
+    rgb_dir = "../data/rgb/"
+    flow_dir = "../data/flow/"
+    metadata_path = "../data/metadata.json"
+    video_dir = "../data/videos/"
+
     # Check for saving directories and create if they don't exist
-    check_and_create_dir("../data/frames")
-    check_and_create_dir("../data/flows")
+    check_and_create_dir(rgb_dir)
+    check_and_create_dir(flow_dir)
 
     # TODO: load video metadata from ../data/metadata.json
-    metadata = load_json("../data/metadata.json")
+    metadata = load_json(metadata_path)
 
     # TODO: loop through the metadata to get the file_name
     for video_data in metadata:
         file_name = video_data["file_name"]
         # TODO: Skip this file if ../data/frames/[file_name].npy and ../data/flows/[file_name].npy both exist
         # TODO: for example: if is_file_here(file_path): continue
-        if is_file_here("../data/frames/" + file_name + ".npy") and is_file_here("../data/flows/" + file_name + ".npy"): continue
+        if is_file_here(rgb_dir + file_name + ".npy") and is_file_here(flow_dir + file_name + ".npy"): continue
         # TODO: load videos from ../data/videos/[file_name].mp4
-        video = str("../data" + file_name + ".mp4")
+        video = str(video_dir + file_name + ".mp4")
         # TODO: process them into rgb frames and optical flows
-        rgb_4d_out_p = str("frames/" + file_name)
-        flow_4d_out_p = str("flows/" + file_name)
-        root_dir = "../data/"
+        rgb_4d_out_p = str(rgb_dir + file_name)
+        flow_4d_out_p = str(flow_dir + file_name)
         # TODO: save rgb frames to ../data/frames/[file_name].npy
         # TODO: save optical flows to ../data/flows/[file_name].npy
-        op.step(rgb_vid_in_p=video, rgb_4d_out_p=rgb_4d_out_p, flow_4d_out_p=flow_4d_out_p, save_path=root_dir)
+        op.step(rgb_vid_in_p=video, rgb_4d_out_p=rgb_4d_out_p, flow_4d_out_p=flow_4d_out_p)
 
 
 if __name__ == "__main__":
