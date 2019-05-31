56b981f9c51d2a6ae1760a5182d7d65669889558
SeanPrendi <sean.prendi@gmail.com>
2019-05-31 12:33:46 -0400

    Bug fix to account for size of Fully Connected Layer input.

--- back-end/www/model/pytorch_ts.py
+++ back-end/www/model/pytorch_ts.py
@@ -6,41 +6,44 @@ class MotionCNN(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.pool = nn.MaxPool3d(kernel_size=3, stride=2)
-        self.renorm = nn.LocalResponseNorm(size=2)
+        self.model = nn.Sequential(
+            nn.Conv3d(in_channels=3, out_channels=96, kernel_size=7, stride=2),
+            nn.MaxPool3d(kernel_size=3, stride=2),
+            nn.ReLU(),
+            nn.LocalResponseNorm(size=2),
 
-        self.conv1 = nn.Conv3d(in_channels=2, out_channels=96, kernel_size=7, stride=2)
-        self.conv2 = nn.Conv3d(in_channels=96, out_channels=256, kernel_size=5, stride=2)
-        self.conv3 = nn.Conv3d(in_channels=256, out_channels=512, kernel_size=3, stride=1)
-        self.conv4 = nn.Conv3d(in_channels=512, out_channels=512, kernel_size=3, stride=1)
-        self.conv5 = nn.Conv3d(in_channels=512, out_channels=512, kernel_size=3, stride=1)
+            nn.Conv3d(in_channels=96, out_channels=256, kernel_size=5,
+                      stride=2),
+            nn.MaxPool3d(kernel_size=3, stride=2),
+            nn.ReLU(),
 
-        self.full_conn1 = nn.Linear(in_features=self.in_dim, out_features=4096)
+            nn.Conv3d(in_channels=256, out_channels=512, kernel_size=3,
+                      stride=1),
+            nn.ReLU(),
+
+            nn.Conv3d(in_channels=512, out_channels=512, kernel_size=3,
+                      stride=1),
+            nn.ReLU(),
+
+            nn.Conv3d(in_channels=512, out_channels=512, kernel_size=3,
+                      stride=1),
+            nn.MaxPool3d(kernel_size=3, stride=2),
+            nn.ReLU(),
+        )
+
+        mock_input = torch.randn(4, 2, 36, 224, 224)
+        mock_output = self.model(mock_input)
+        flattened_outputed = torch.flatten(mock_output, start_dim=1)
+        fc_in_dim = flattened_outputed[1]
+
+        self.full_conn1 = nn.Linear(in_features=fc_in_dim, out_features=4096)
         self.full_conn2 = nn.Linear(in_features=4096, out_features=2048)
         self.full_conn3 = nn.Linear(in_features=2048, out_features=2)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.pool(x)
-        x = F.relu(x)
-        x = self.renorm(x)
-
-        x = self.conv2(x)
-        self.pool(x)
-        x = F.relu(x)
-
-        x = self.conv3(x)
-        x = F.relu(x)
-
-        x = self.conv4(x)
-        x = F.relu(x)
-
-        x = self.conv5(x)
-        x = self.pool(x)
-        x = F.relu(x)
+        x = self.model(x)
 
         x = torch.flatten(x, start_dim=1)  # Flattens layers without losing batches
-        self.in_dim = x[1]
 
         x = self.full_conn1(x)
         x = F.dropout(x)
@@ -56,44 +59,45 @@ class SpatialCNN(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.pool = nn.MaxPool3d(kernel_size=3, stride=2)
-        self.renorm = nn.LocalResponseNorm(size=2)
+        self.model = nn.Sequential(
+            nn.Conv3d(in_channels=3, out_channels=96, kernel_size=7, stride=2),
+            nn.MaxPool3d(kernel_size=3, stride=2),
+            nn.ReLU(),
+            nn.LocalResponseNorm(size=2),
 
-        self.conv1 = nn.Conv3d(in_channels=3, out_channels=96, kernel_size=7, stride=2)
-        self.conv2 = nn.Conv3d(in_channels=96, out_channels=256, kernel_size=5, stride=2)
-        self.conv3 = nn.Conv3d(in_channels=256, out_channels=512, kernel_size=3, stride=1)
-        self.conv4 = nn.Conv3d(in_channels=512, out_channels=512, kernel_size=3, stride=1)
-        self.conv5 = nn.Conv3d(in_channels=512, out_channels=512, kernel_size=3, stride=1)
+            nn.Conv3d(in_channels=96, out_channels=256, kernel_size=5,
+                      stride=2),
+            nn.MaxPool3d(kernel_size=3, stride=2),
+            nn.ReLU(),
+            nn.LocalResponseNorm(size=2),
 
-        self.full_conn1 = nn.Linear(in_features=self.in_dim, out_features=4096)
+            nn.Conv3d(in_channels=256, out_channels=512, kernel_size=3,
+                      stride=1),
+            nn.ReLU(),
+
+            nn.Conv3d(in_channels=512, out_channels=512, kernel_size=3,
+                      stride=1),
+            nn.ReLU(),
+
+            nn.Conv3d(in_channels=512, out_channels=512, kernel_size=3,
+                      stride=1),
+            nn.MaxPool3d(kernel_size=3, stride=2),
+            nn.ReLU(),
+        )
+
+        mock_input = torch.randn(4, 3, 36, 224, 224)
+        mock_output = self.model(mock_input)
+        flattened_outputed = torch.flatten(mock_output, start_dim=1)
+        fc_in_dim = flattened_outputed[1]
+
+        self.full_conn1 = nn.Linear(in_features=fc_in_dim, out_features=4096)
         self.full_conn2 = nn.Linear(in_features=4096, out_features=2048)
         self.full_conn3 = nn.Linear(in_features=2048, out_features=2)
 
-
-
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.pool(x)
-        x = F.relu(x)
-        x = self.renorm(x)
-
-        x = self.conv2(x)
-        self.pool(x)
-        x = F.relu(x)
-        x = self.renorm(x)
-
-        x = self.conv3(x)
-        x = F.relu(x)
-
-        x = self.conv4(x)
-        x = F.relu(x)
-
-        x = self.conv5(x)
-        x = self.pool(x)
-        x = F.relu(x)
+        x = self.model(x)
 
         x = torch.flatten(x, start_dim=1)  # Flattens layers without losing batches
-        self.in_dim = x[1]
 
         x = self.full_conn1(x)
         x = F.dropout(x)
