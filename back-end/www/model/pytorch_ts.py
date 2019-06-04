6f1b79f8af9392449829d1a884ac78a4f657c8f4
SeanPrendi <sean.prendi@gmail.com>
2019-06-04 09:07:32 -0400

    Changed convolution functions to 2 dimensional.

--- back-end/www/model/pytorch_ts.py
+++ back-end/www/model/pytorch_ts.py
@@ -7,33 +7,33 @@ class MotionCNN(nn.Module):
         super().__init__()
 
         self.model = nn.Sequential(
-            nn.Conv3d(in_channels=2, out_channels=96, kernel_size=7, stride=2),
-            nn.MaxPool3d(kernel_size=3, stride=1),
+            nn.Conv2d(in_channels=2, out_channels=96, kernel_size=7, stride=2),
+            nn.MaxPool2d(kernel_size=3, stride=1),
             nn.ReLU(),
             nn.LocalResponseNorm(size=2),
 
-            nn.Conv3d(in_channels=96, out_channels=256, kernel_size=5,
+            nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5,
                       stride=1),
-            nn.MaxPool3d(kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=3, stride=1),
             nn.ReLU(),
 
-            nn.Conv3d(in_channels=256, out_channels=512, kernel_size=3,
-                      stride=1),
-            nn.ReLU(),
-
-            nn.Conv3d(in_channels=512, out_channels=512, kernel_size=3,
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3,
                       stride=1),
             nn.ReLU(),
 
-            nn.Conv3d(in_channels=512, out_channels=512, kernel_size=3,
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3,
+                      stride=1),
+            nn.ReLU(),
+
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3,
                       stride=1),
             nn.ReLU(),
         )
 
-        mock_input = torch.randn(4, 2, 36, 224, 224)
+        mock_input = torch.randn(4, 2, 224, 224)
         mock_output = self.model(mock_input)
         flattened_output = torch.flatten(mock_output, start_dim=1)
-        fc_in_dim = ((flattened_output[1]).shape).item() # Get number of nodes from flattened value's size, then convert 0 dim tensor to integer
+        fc_in_dim = flattened_output.shape[1] # Get number of nodes from flattened value's size, then convert 0 dim tensor to integer
 
         self.full_conn1 = nn.Linear(in_features=fc_in_dim, out_features=4096)
         self.full_conn2 = nn.Linear(in_features=4096, out_features=2048)
@@ -59,34 +59,34 @@ class SpatialCNN(nn.Module):
         super().__init__()
 
         self.model = nn.Sequential(
-            nn.Conv3d(in_channels=3, out_channels=96, kernel_size=7, stride=2),
-            nn.MaxPool3d(kernel_size=3, stride=1),
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=7, stride=2),
+            nn.MaxPool2d(kernel_size=3, stride=1),
             nn.ReLU(),
             nn.LocalResponseNorm(size=2),
 
-            nn.Conv3d(in_channels=96, out_channels=256, kernel_size=5,
+            nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5,
                       stride=1),
-            nn.MaxPool3d(kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=3, stride=1),
             nn.ReLU(),
             nn.LocalResponseNorm(size=2),
 
-            nn.Conv3d(in_channels=256, out_channels=512, kernel_size=3,
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3,
                       stride=1),
             nn.ReLU(),
 
-            nn.Conv3d(in_channels=512, out_channels=512, kernel_size=3,
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3,
                       stride=1),
             nn.ReLU(),
 
-            nn.Conv3d(in_channels=512, out_channels=512, kernel_size=3,
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3,
                       stride=1),
             nn.ReLU(),
         )
 
-        mock_input = torch.randn(4, 3, 36, 224, 224)
+        mock_input = torch.randn(4, 3, 224, 224)
         mock_output = self.model(mock_input)
         flattened_output = torch.flatten(mock_output, start_dim=1)
-        fc_in_dim = ((flattened_output[1]).shape).item() # Get number of nodes from flattened value's size, then convert 0 dim tensor to integer
+        fc_in_dim = flattened_output.shape[1] # Get number of nodes from flattened value's size, then convert 0 dim tensor to integer
 
 
         self.full_conn1 = nn.Linear(in_features=fc_in_dim, out_features=4096)
