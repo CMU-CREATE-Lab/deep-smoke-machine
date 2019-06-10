ed07578ecd08edb1b90ea327504c4d5d01d0bb0b
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2019-06-10 16:31:25 -0400

    Add more background scripts.

---
+++ back-end/www/bg_train_i3d_rgb.sh
@@ -0,0 +1,15 @@
+# Delete existing screen
+for session in $(sudo screen -ls | grep -o '[0-9]*.train_i3d_rgb')
+do
+  sudo screen -S "${session}" -X quit
+  sleep 2
+done
+
+# Delete the log
+sudo rm screenlog.0
+
+# For python in conda env
+sudo screen -dmSL "train_i3d_rgb" bash -c "export PATH='/opt/miniconda3/bin:$PATH'; . '/opt/miniconda3/etc/profile.d/conda.sh'; conda activate deep-smoke-machine; python train.py i3d-rgb"
+
+# List screens
+sudo screen -ls
