a5852fc0d4afe22c465c6ff4f516132e41d6cdab
SeanPrendi <sean.prendi@gmail.com>
2019-07-26 09:57:02 -0400

    Update architecture to reflect flattening dimensions of inputs.

--- back-end/www/model/pytorch_ts.py
+++ back-end/www/model/pytorch_ts.py
@@ -7,31 +7,27 @@ class MotionCNN(nn.Module):
         super().__init__()
 
         self.model = nn.Sequential(
-            nn.Conv2d(in_channels=2, out_channels=96, kernel_size=7, stride=2),
+            nn.Conv2d(in_channels=72, out_channels=96, kernel_size=7, stride=2),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.ReLU(),
             nn.LocalResponseNorm(size=2),
 
-            nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5,
-                      stride=2),
+            nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, stride=2),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.ReLU(),
 
-            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3,
-                      stride=1),
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1),
             nn.ReLU(),
 
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3,
-                      stride=1),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1),
             nn.ReLU(),
 
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3,
-                      stride=1),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.ReLU(),
         )
 
-        mock_input = torch.randn(4, 2, 224, 224)
+        mock_input = torch.randn(64, 72, 224, 224)
         mock_output = self.model(mock_input)
         flattened_output = torch.flatten(mock_output, start_dim=1)
         fc_in_dim = flattened_output.shape[1] # Get number of nodes from flattened value's size, then convert 0 dim tensor to integer
@@ -39,6 +35,7 @@ class MotionCNN(nn.Module):
         self.full_conn1 = nn.Linear(in_features=fc_in_dim, out_features=4096)
         self.full_conn2 = nn.Linear(in_features=4096, out_features=2048)
         self.full_conn3 = nn.Linear(in_features=2048, out_features=2)
+        #self.full_conn3 = nn.Linear(in_features=4096, out_features=2)
 
     def forward(self, x):
         x = self.model(x)
@@ -65,19 +62,16 @@ class SpatialCNN(nn.Module):
             nn.ReLU(),
             nn.LocalResponseNorm(size=2),
 
-            nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5,
-                      stride=2),
+            nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, stride=2),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.ReLU(),
             nn.LocalResponseNorm(size=2),
 
-            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3,
-                      stride=1),
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1),
             nn.ReLU(),
 
-            #nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3,
-            #          stride=1),
-            #nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1),
+            nn.ReLU(),
 
             #nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3,
             #          stride=1),
@@ -103,10 +97,10 @@ class SpatialCNN(nn.Module):
         x = torch.flatten(x, start_dim=1)  # Flattens layers without losing batches
 
         x = self.full_conn1(x)
-        # x = F.dropout(x)
+        #x = F.dropout(x)
 
         #x = self.full_conn2(x)
-        # x = F.dropout(x)
+        #x = F.dropout(x)
 
         x = self.full_conn3(x)
 
