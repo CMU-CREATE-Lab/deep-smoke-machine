7e6622b430081d03f212a60c6e01c4e3b9cfd5c9
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2019-09-17 16:26:04 -0400

    Add color jittering function.

--- back-end/www/show_video_transforms.py
+++ back-end/www/show_video_transforms.py
@@ -3,20 +3,23 @@ import matplotlib
 matplotlib.use("TkAgg") # a fix for Mac OS X error
 from optical_flow.optical_flow import OpticalFlow
 from torchvision.transforms import Compose
-from video_transforms import *
-from smoke_video_dataset import *
+from video_transforms import RandomCrop, RandomHorizontalFlip, ColorJitter
+import numpy as np
+
 
 def main(argv):
     if len(argv) < 2:
         print("Usage: python test_video_transforms.py [video_file_path]")
         return
     op = OpticalFlow(rgb_vid_in_p=argv[1])
-    rgb_4d = op.vid_to_frames()
-    rf = RandomHorizontalFlip()
-    #cj = ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=(-0.1, 0.1))
-    T = Compose([RandomCrop(180), rf])
+    rgb_4d = op.vid_to_frames().astype(np.uint8) # ColorJitter need uint8
+    rc = RandomCrop(180)
+    rf = RandomHorizontalFlip(p=0.5)
+    cj = ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=(-0.1, 0.1))
+    T = Compose([rc, rf, cj])
     rgb_4d = T(rgb_4d)
     op.frames_to_vid(rgb_4d, "../data/transformed.mp4")
+
 
 if __name__ == "__main__":
     main(sys.argv)
--- back-end/www/video_transforms.py
+++ back-end/www/video_transforms.py
@@ -1,10 +1,14 @@
 """
 This file is obtained from https://github.com/piergiaj/pytorch-i3d
 """
-
 import numpy as np
 import numbers
 import random
+import warnings
+import types
+import opencv_functional as F
+from torchvision.transforms import Compose
+
 
 class RandomCrop(object):
     """Crop the given video sequences (t x h x w) at a random location.
@@ -13,7 +17,6 @@ class RandomCrop(object):
             int instead of sequence like (h, w), a square crop (size, size) is
             made.
     """
-
     def __init__(self, size):
         if isinstance(size, numbers.Number):
             self.size = (int(size), int(size))
@@ -48,6 +51,7 @@ class RandomCrop(object):
     def __repr__(self):
         return self.__class__.__name__ + '(size={0})'.format(self.size)
 
+
 class CenterCrop(object):
     """Crops the given seq Images at the center.
     Args:
@@ -55,7 +59,6 @@ class CenterCrop(object):
             int instead of sequence like (h, w), a square crop (size, size) is
             made.
     """
-
     def __init__(self, size):
         if isinstance(size, numbers.Number):
             self.size = (int(size), int(size))
@@ -86,7 +89,6 @@ class RandomHorizontalFlip(object):
     Args:
         p (float): probability of the image being flipped. Default value is 0.5
     """
-
     def __init__(self, p=0.5):
         self.p = p
 
@@ -104,3 +106,121 @@ class RandomHorizontalFlip(object):
 
     def __repr__(self):
         return self.__class__.__name__ + '(p={})'.format(self.p)
+
+
+class Lambda(object):
+    """Apply a user-defined lambda as a transform.
+    Args:
+        lambd (function): Lambda/function to be used for transform.
+    """
+    def __init__(self, lambd):
+        assert isinstance(lambd, types.LambdaType)
+        self.lambd = lambd
+
+    def __call__(self, img):
+        return self.lambd(img)
+
+    def __repr__(self):
+        return self.__class__.__name__ + '()'
+
+
+class ColorJitter(object):
+    """Randomly change the brightness, contrast and saturation of a sequence of images.
+    Args:
+        brightness (float or tuple of float (min, max)): How much to jitter brightness.
+            brightness_factor is chosen uniformly from [max(0, 1 - brightness), 1 + brightness]
+            or the given [min, max]. Should be non negative numbers.
+        contrast (float or tuple of float (min, max)): How much to jitter contrast.
+            contrast_factor is chosen uniformly from [max(0, 1 - contrast), 1 + contrast]
+            or the given [min, max]. Should be non negative numbers.
+        saturation (float or tuple of float (min, max)): How much to jitter saturation.
+            saturation_factor is chosen uniformly from [max(0, 1 - saturation), 1 + saturation]
+            or the given [min, max]. Should be non negative numbers.
+        hue (float or tuple of float (min, max)): How much to jitter hue.
+            hue_factor is chosen uniformly from [-hue, hue] or the given [min, max].
+            Should have 0<= hue <= 0.5 or -0.5 <= min <= max <= 0.5.
+    """
+    def __init__(self, brightness=0, contrast=0, saturation=0, hue=0):
+        self.brightness = self._check_input(brightness, 'brightness')
+        self.contrast = self._check_input(contrast, 'contrast')
+        self.saturation = self._check_input(saturation, 'saturation')
+        self.hue = self._check_input(hue, 'hue', center=0, bound=(-0.5, 0.5),
+                                     clip_first_on_zero=False)
+        if self.saturation is not None:
+            warnings.warn('Saturation jitter enabled. Will slow down loading immensely.')
+        if self.hue is not None:
+            warnings.warn('Hue jitter enabled. Will slow down loading immensely.')
+
+    def _check_input(self, value, name, center=1, bound=(0, float('inf')), clip_first_on_zero=True):
+        if isinstance(value, numbers.Number):
+            if value < 0:
+                raise ValueError("If {} is a single number, it must be non negative.".format(name))
+            value = [center - value, center + value]
+            if clip_first_on_zero:
+                value[0] = max(value[0], 0)
+        elif isinstance(value, (tuple, list)) and len(value) == 2:
+            if not bound[0] <= value[0] <= value[1] <= bound[1]:
+                raise ValueError("{} values should be between {}".format(name, bound))
+        else:
+            raise TypeError("{} should be a single number or a list/tuple with length 2.".format(name))
+
+        # if value is 0 or (1., 1.) for brightness/contrast/saturation
+        # or (0., 0.) for hue, do nothing
+        if value[0] == value[1] == center:
+            value = None
+        return value
+
+    @staticmethod
+    def get_params(brightness, contrast, saturation, hue):
+        """Get a randomized transform to be applied on image.
+        Arguments are same as that of __init__.
+        Returns:
+            Transform which randomly adjusts brightness, contrast and
+            saturation in a random order.
+        """
+        transforms = []
+
+        if brightness is not None:
+            brightness_factor = random.uniform(brightness[0], brightness[1])
+            transforms.append(Lambda(lambda img: F.adjust_brightness(img, brightness_factor)))
+
+        if contrast is not None:
+            contrast_factor = random.uniform(contrast[0], contrast[1])
+            transforms.append(Lambda(lambda img: F.adjust_contrast(img, contrast_factor)))
+
+        if saturation is not None:
+            saturation_factor = random.uniform(saturation[0], saturation[1])
+            transforms.append(Lambda(lambda img: F.adjust_saturation(img, saturation_factor)))
+
+        if hue is not None:
+            hue_factor = random.uniform(hue[0], hue[1])
+            transforms.append(Lambda(lambda img: F.adjust_hue(img, hue_factor)))
+
+        random.shuffle(transforms)
+        transform = Compose(transforms)
+
+        return transform
+
+    def __call__(self, imgs):
+        """
+        Args:
+            imgs (numpy ndarray): Input image sequence (time*height*width*channel).
+        Returns:
+            numpy ndarray: Color jittered image sequence.
+        """
+        transform = self.get_params(self.brightness, self.contrast,
+                                    self.saturation, self.hue)
+        # Apply to all images
+        jittered_imgs = []
+        for I in imgs:
+            jittered_imgs.append(transform(I))
+
+        return np.array(jittered_imgs)
+
+    def __repr__(self):
+        format_string = self.__class__.__name__ + '('
+        format_string += 'brightness={0}'.format(self.brightness)
+        format_string += ', contrast={0}'.format(self.contrast)
+        format_string += ', saturation={0}'.format(self.saturation)
+        format_string += ', hue={0})'.format(self.hue)
+        return format_string
