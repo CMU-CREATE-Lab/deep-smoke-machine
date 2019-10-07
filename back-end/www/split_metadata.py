5da8085379304b42edde4136410fd0e836bf9d18
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2019-10-07 15:24:23 -0400

    Finish data splitting.

--- back-end/www/split_metadata.py
+++ back-end/www/split_metadata.py
@@ -4,69 +4,121 @@ import json
 import numpy as np
 from util import *
 
-def random_split(vm_dict):
-    target_keys = list(vm_dict.keys())
-    np.random.shuffle(target_keys)
+def split_and_save_data(vm, target_key_type, method="assign"):
+    # Index metadata by date or camera
+    vm_dict = defaultdict(list)
+    for v in vm:
+        k = to_key(v, target_key_type)
+        vm_dict[k].append(v)
+
+    p = "../data/split/"
+    check_and_create_dir(p)
+    if method == "random":
+        vm_train, vm_valid, vm_test = split(vm_dict, target_key_type)
+        save_json(vm_valid, p+"metadata_validation_random_split_by_"+target_key_type+".json")
+        save_json(vm_test, p+"metadata_test_random_split_by_"+target_key_type+".json")
+        save_json(vm_train, p+"metadata_train_random_split_by_"+target_key_type+".json")
+    elif method == "assign":
+        if target_key_type == "camera":
+            three_splits = [
+                {
+                    "train": ["0-0", "0-3", "0-6", "0-8", "0-12", "0-2", "0-5", "0-9", "0-11", "0-14"],
+                    "valid": ["0-4", "0-10"],
+                    "test": ["1-0", "2-0", "2-1", "2-2", "0-1", "0-7", "0-13"]
+                }, {
+                    "train": ["0-1", "0-4", "0-7", "0-10", "0-13", "0-0", "0-3", "0-6", "0-8", "0-12"],
+                    "valid": ["0-5", "0-11"],
+                    "test": ["1-0", "2-0", "2-1", "2-2", "0-2", "0-9", "0-14"]
+                }, {
+                    "train": ["0-2", "0-5", "0-9", "0-11", "0-14", "0-1", "0-4", "0-7", "0-10", "0-13"],
+                    "valid": ["0-3", "0-8"],
+                    "test": ["1-0", "2-0", "2-1", "2-2", "0-0", "0-6", "0-12"]
+                }]
+            for i in range(len(three_splits)):
+                s = three_splits[i]
+                vm_train, vm_valid, vm_test = split(vm_dict, target_key_type,
+                        train_key=s["train"], valid_key=s["valid"], test_key=s["test"])
+                save_json(vm_valid, p+"metadata_validation_split_"+str(i)+"_by_"+target_key_type+".json")
+                save_json(vm_test, p+"metadata_test_split_"+str(i)+"_by_"+target_key_type+".json")
+                save_json(vm_train, p+"metadata_train_split_"+str(i)+"_by_"+target_key_type+".json")
+        elif target_key_type == "date":
+            target_keys = list(vm_dict.keys())
+            target_keys = sorted(target_keys)[::-1]
+            train_key, valid_key, test_key = divide_list(target_keys, frac_valid=0.1, frac_test=0.25)
+            vm_train, vm_valid, vm_test = split(vm_dict, target_key_type,
+                    train_key=train_key, valid_key=valid_key, test_key=test_key)
+            save_json(vm_valid, p+"metadata_validation_split_by_"+target_key_type+".json")
+            save_json(vm_test, p+"metadata_test_split_by_"+target_key_type+".json")
+            save_json(vm_train, p+"metadata_train_split_by_"+target_key_type+".json")
+
+
+def divide_list(target_keys, frac_valid=0.1, frac_test=0.3):
     n_keys = len(target_keys)
-    n_valid = int(n_keys*0.1) # 10% for validation
-    n_test = int(n_keys*0.3) # 30% for testing
-    target_keys_valid = target_keys[:n_valid]
-    target_keys_test = target_keys[n_valid:n_valid+n_test]
+    n_valid = int(n_keys*frac_valid)
+    n_test = int(n_keys*frac_test)
+    test_key = target_keys[:n_test]
+    valid_key = target_keys[n_test:n_valid+n_test]
+    train_key = target_keys[n_valid+n_test:]
+    return (train_key, valid_key, test_key)
+
+
+def split(vm_dict, target_key_type, train_key=None, valid_key=None, test_key=None):
+    if train_key is None or valid_key is None or test_key is None:
+        target_keys = list(vm_dict.keys())
+        np.random.shuffle(target_keys)
+        # 10% for validation, 30% for testing
+        train_key, valid_key, test_key = divide_list(target_keys, frac_valid=0.1, frac_test=0.3)
     vm_valid = []
     vm_test = []
     vm_train = []
     for d in vm_dict:
-        if d in target_keys_valid:
+        if d in valid_key:
             vm_valid += vm_dict[d]
-        elif d in target_keys_test:
+        elif d in test_key:
             vm_test += vm_dict[d]
-        else:
+        elif d in train_key:
             vm_train += vm_dict[d]
-    print("Size of validation set:", len(vm_valid))
-    print("Size of test set:", len(vm_test))
-    print("Size of training set:", len(vm_train))
+    print("\nTraining:")
+    print_distribution(vm_train, target_key_type=target_key_type)
+    print("\nValidation:")
+    print_distribution(vm_valid, target_key_type=target_key_type)
+    print("\nTesting:")
+    print_distribution(vm_test, target_key_type=target_key_type)
+    n = len(vm_valid) + len(vm_test) + len(vm_train)
+    print("\nSize of validation set: %d (%.2f)" % (len(vm_valid), len(vm_valid)/n))
+    print("Size of test set: %d (%.2f)" % (len(vm_test), len(vm_test)/n))
+    print("Size of training set: %d (%.2f)" % (len(vm_train), len(vm_train)/n))
     return (vm_train, vm_valid, vm_test)
 
 
-def print_distribution(vm):
-    count_vm_by_date = defaultdict(lambda: defaultdict(int))
-    count_vm_by_cam = defaultdict(lambda: defaultdict(int))
+def print_distribution(vm, target_key_type):
+    count_vm = defaultdict(lambda: defaultdict(int))
     pos = [47, 23, 19, 15]
     neg = [32, 20, 16, 12]
     gold_pos = [47]
     gold_neg = [32]
     for v in vm:
-        cam, date = to_keys(v)
+        k = to_key(v, target_key_type)
         label = v["label_state_admin"] # TODO: need to change this to label_state in the future
-        # By date
         if label in pos:
-            count_vm_by_date[date]["pos"] += 1
+            count_vm[k]["pos"] += 1
         elif label in neg:
-            count_vm_by_date[date]["neg"] += 1
+            count_vm[k]["neg"] += 1
         if label in gold_pos:
-            count_vm_by_date[date]["gold_pos"] += 1
+            count_vm[k]["gold_pos"] += 1
         elif label in gold_neg:
-            count_vm_by_date[date]["gold_neg"] += 1
-        # By camera
-        if label in pos:
-            count_vm_by_cam[cam]["pos"] += 1
-        elif label in neg:
-            count_vm_by_cam[cam]["neg"] += 1
-        if label in gold_pos:
-            count_vm_by_cam[cam]["gold_pos"] += 1
-        elif label in gold_neg:
-            count_vm_by_cam[cam]["gold_neg"] += 1
-    #print(json.dumps(count_vm_by_date, indent=4))
-    print(json.dumps(count_vm_by_cam, indent=4))
+            count_vm[k]["gold_neg"] += 1
+    print(json.dumps(count_vm, indent=4))
 
 
-def to_keys(v):
+def to_key(v, target_key_type):
     # The file name contains information about camera, date, and bounding box
     # We can use this as the key of the dataset for separating training, validation, and test sets
     key = v["file_name"].split("-")
-    cam = "-".join(key[0:2])
-    date = "-".join(key[2:5])
-    return (cam, date)
+    if target_key_type == "camera":
+        return "-".join(key[0:2])
+    elif target_key_type == "date":
+        return "-".join(key[2:5])
 
 
 # Split metadata into training, validation, and test sets
@@ -80,29 +132,11 @@ def main(argv):
         print("Must confirm by running: python split_metadata.py confirm")
         return
 
-    # Index metadata by date, camera, or bounding box
     vm = load_json("../data/metadata.json")
-    vm_by_date = defaultdict(list)
-    vm_by_cam = defaultdict(list)
-    for v in vm:
-        cam, date = to_keys(v)
-        vm_by_date[date].append(v)
-        vm_by_cam[cam].append(v)
-
-    # Randomly split dataset
-    #vm_train, vm_valid, vm_test = random_split(vm_by_date)
-    vm_train, vm_valid, vm_test = random_split(vm_by_cam)
-    print("\nTraining:")
-    print_distribution(vm_train)
-    print("\nValidation:")
-    print_distribution(vm_valid)
-    print("\nTesting:")
-    print_distribution(vm_test)
-
-    # Save the splits
-    save_json(vm_valid, "../data/metadata_validation.json")
-    save_json(vm_test, "../data/metadata_test.json")
-    save_json(vm_train, "../data/metadata_train.json")
+    #method = "random"
+    method = "assign"
+    split_and_save_data(vm, "date", method=method)
+    split_and_save_data(vm, "camera", method=method)
 
 
 if __name__ == "__main__":
