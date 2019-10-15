77aa8ed1d93089cf435a37dc502e806eb528064e
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2019-10-15 18:22:14 -0400

    Add a script for perturbing frames.

---
+++ back-end/www/perturb_frames.py
@@ -0,0 +1,29 @@
+import os
+import sys
+from util import *
+import numpy as np
+from multiprocessing import Pool
+
+rgb_perturb_dir = "../data/rgb_perturb/"
+rgb_dir = "../data/rgb/"
+
+
+# Perturb rgb frames and save them
+def main(argv):
+    num_workers = 6
+    check_and_create_dir(rgb_perturb_dir)
+    file_list = get_all_file_names_in_folder(rgb_dir)
+    p = Pool(num_workers)
+    p.map(perturb, file_list)
+    print("Done")
+
+
+def perturb(f):
+    print("Process: " + f)
+    frames = np.load(rgb_dir + f)
+    frames_perturb = np.random.permutation(frames)
+    np.save(rgb_perturb_dir + f, frames_perturb)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
