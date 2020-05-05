f1c07e728eeb757656849278d34dcdd82e23d0aa
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2020-05-05 15:54:13 -0400

    Add a new script for downloading all videos.

--- back-end/www/download_all_videos.py
+++ back-end/www/download_all_videos.py
@@ -5,19 +5,19 @@ from util import *
 
 # Download all videos in the metadata json file
 def main(argv):
-    vm = load_json("../data/metadata.json")
-    video_root_path = "../data/videos/"
+    vm = load_json("../data/metadata_all.json")["data"]
+    video_root_path = "../data/videos_all/"
     check_and_create_dir(video_root_path)
     problem_video_ids = []
     for v in vm:
-        # Do not download videos with bad data
-        if v["label_state"] == -2 or v["label_state_admin"] == -2:
-            continue
         file_path = video_root_path + v["file_name"] + ".mp4"
         if is_file_here(file_path): continue # skip if file exists
         print("Download video", v["id"])
         try:
-            urllib.request.urlretrieve(v["url_root"] + v["url_part"], file_path)
+            url = v["url_root"] + v["url_part"]
+            url = url.replace("width=180", "width=320")
+            url = url.replace("height=180", "height=320")
+            urllib.request.urlretrieve(url, file_path)
         except:
             print("\tError downloading video", v["id"])
             problem_video_ids.append(v["id"])
